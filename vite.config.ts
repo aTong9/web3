b7d670f7bf0e1d45efb72f42441f3c548906e4a3
aTong9 <aTong9@users.noreply.github.com>
2026-01-21 03:11:49 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  // base: process.env.NODE_ENV === 'production' ? '/web3/' : '/',
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
